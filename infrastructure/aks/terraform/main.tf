63571561bd0f0cb94678f72222b541a1901cbc0b
humzgod <tracert.hackstuff@gmail.com>
2024-05-23 09:40:56 +0000

    enh: Add depends on statements in terraform module declarations.
    This should ensure the provisioning sequence since resources will not be creating before their dependencies are ready.

--- infrastructure/aks/terraform/main.tf
+++ infrastructure/aks/terraform/main.tf
@@ -9,6 +9,22 @@ module "azuread_group" {
   source = "./modules/entra_id_group"
   admin_group_name  = var.admin_group_name
   cluster_name = var.cluster_name
+  depends_on = [
+    module.azurerm_resource_group
+  ]
+}
+
+module "azurerm_virtual_network" {
+  source = "./modules/virtual_network"
+  project_name = var.cluster_name
+  location = var.location
+  resource_group_name = var.resource_group_name
+  vnet_name = var.vnet_name
+  vnet_address_space = var.vnet_address_space
+  subnets = var.subnets
+  depends_on = [
+    module.azurerm_resource_group
+  ]
 }
 
 module "azurerm_kubernetes_cluster" {
@@ -25,4 +41,9 @@ module "azurerm_kubernetes_cluster" {
   node_os_disk_size_gb = var.node_os_disk_size_gb
   admin_username = var.admin_username
   client_id = var.client_id
+  depends_on = [
+    module.azuread_group,
+    module.azurerm_virtual_network,
+    module.azurerm_resource_group
+  ]
 }
--- infrastructure/aks/terraform/modules/resource_groups/main.tf
+++ infrastructure/aks/terraform/modules/resource_groups/main.tf
@@ -1,10 +1,5 @@
 resource "azurerm_resource_group" "azure_resource_group" {
   name     = var.resource_group_name
   location = var.location
-
   tags = var.tags
-
-  provisioner "local-exec" {
-    command = "sleep 40"
-  }
 }
